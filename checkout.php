82b602ef3f7d18ebe40814e15e23c8f277a5edf4
Tenzin <dhontenzin@gmail.com>
2021-12-26 13:31:48 +0100

    stripe- so many things to add, like cart status, removing frm cart & adding in profile?

--- checkout.php
+++ checkout.php
@@ -5,26 +5,26 @@
 <?php 
     if(Session::get('cusLogin') == false) header('Location: login.php'); 
     
-    $getCart = $cart->getAllCart();
+    $getCart           = $cart->getAllCart();
     if($getCart) $cart = $getCart->fetch();
 
-    $getCombinedCusDetails = $user->getCombinedCusDetails(Session::get('cusId'));
+    $getCombinedCusDetails           = $user->getCombinedCusDetails(Session::get('cusId'));
     if($getCombinedCusDetails) $rows = $getCombinedCusDetails->fetch();
 
-    $token = $_POST['stripeToken'];
-    $cusName = $rows['customerName'];
-    $token_card_type = $_POST['stripeTokenType'];
-    $phone = $rows['phone'];
-    $email = $_POST['stripeEmail'];
-    $address = $rows['address'];
-    $amount = $cart['amount']*$cart['quantity'];
-    $desc = $_cart['courseName'];
-    $charge = \Stripe\Charge::create([
-        "amount" => str_replace(",","",Session::get('totalPrice')) * 100,
-        "currency" => 'eur',
-        "description"=>$desc,
-        "source"=> $token,
-      ]);
+    $token              = $_POST['stripeToken'];
+    $cusName            = $rows['customerName'];
+    $token_card_type    = $_POST['stripeTokenType'];
+    $phone              = $rows['phone'];
+    $email              = $_POST['stripeEmail'];
+    $address            = $rows['address'];
+    $amount             = $cart['amount']*$cart['quantity'];
+    $desc               = $_cart['courseName'];
+    $charge             = \Stripe\Charge::create([
+                            "amount" => str_replace(",","",Session::get('totalPrice')) * 100,
+                            "currency" => 'eur',
+                            "description"=>$desc,
+                            "source"=> $token,
+                        ]);
   
       if($charge){
           var_dump($getCombinedCusDetails);
