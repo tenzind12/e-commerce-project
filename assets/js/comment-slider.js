bf143dde1a317e6e41bef4b622104506f445d69c
Tenzin <dhontenzin@gmail.com>
2022-01-10 19:52:45 +0100

    added dots slider functionality

--- assets/js/comment-slider.js
+++ assets/js/comment-slider.js
@@ -2,30 +2,46 @@ const slider = () => {
   const slides = document.querySelectorAll(".c__slide");
   const dotContainer = document.querySelector(".dots");
 
-  let curSlide = 0;
-  const maxSlide = slides.length;
-
-  function createDots() {
+  (function createDots() {
     slides.forEach((_, i) => {
       dotContainer.insertAdjacentHTML(
         "beforeend",
-        `<button class='dots__dot[data-slide=${i}"]'></button>`
+        `<button class='dot dots__dot' data-slide='${i}'></button>` //dot class for css
       );
     });
-  }
+  })();
 
-  // auto slider
-  setInterval(() => {
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  const goToSlide = (slide) => {
+    slides.forEach((s, i) => {
+      s.style.transform = `translateX(${100 * (i - slide)}%)`;
+    });
+  };
+
+  // slide => first to start from 0
+  goToSlide(0);
+
+  const nextSlide = () => {
     if (curSlide === maxSlide - 1) {
       curSlide = 0;
     } else {
       curSlide++;
     }
-    slides.forEach((s, i) => {
-      s.style.transform = `translateX(${100 * (i - curSlide)}%)`;
-    });
-  }, 5000);
 
-  slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
+    goToSlide(curSlide);
+  };
+
+  // auto slider
+  setInterval(() => nextSlide(), 5000);
+
+  // button slider
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots__dot")) {
+      const slideNumber = e.target.dataset.slide;
+      goToSlide(slideNumber);
+    }
+  });
 };
 slider();
