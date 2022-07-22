2646aedca9df743b1a592581c7a1d0bcb4abf8d7
Tenzin Dekyong <dhontenzin@gmail.com>
2022-07-22 12:57:07 +0200

    stripe config init

--- core/config/config.php
+++ core/config/config.php
@@ -1,5 +1,11 @@
 <?php
-    define("DB_HOST", "localhost");
-    define("DB_USER", "root");
-    define("DB_PASS", "");
-    define("DB_NAME", "project_e_commerce");
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "project_e_commerce");
+
+
+// stripe
+define('STRIPE_API_KEY', 'sk_test_51KAuPUAKBlCe0kMMHihYXBQQ38pZkaOxIGEDCaSTFstiWI4prNkee0djFNCZ5yRR05iZGV1QpEEeDUopwkNjZUvq00JrvJ7u1a');
+
+define('STRIPE_PUBLISHABLE_KEY', 'pk_test_51KAuPUAKBlCe0kMM28dRBK87PLmH9wZicPdMZCAXopbyM4yRnhy4BTWOL4BdcXoCcHQGRtbc5OgYppgN5qaYo1IP00WXvCGocO');
